7be5c8baabc50b5a1e3fe3d3d784239947cdaefa
Hari Nv <harinv18@gmail.com>
2024-12-17 19:41:20 +0530

    url

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ const cors = require("cors");
 const app = express();
 
 // Middleware setup
-app.use(express.json());  
+app.use(express.json());
 app.use(
   cors({
-    origin: "https://product-frontend-amber.vercel.app/", 
-    methods: ["GET", "POST", "PUT", "DELETE"],  
-    credentials: true,  
+    origin: "https://product-frontend-ten.vercel.app/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 
 // MongoDB connection configuration
-const MONGO_URI = "mongodb+srv://harinv18:zPWAzE6dMaLy6JpU@product-list.swerw.mongodb.net/";
+const MONGO_URI =
+  "mongodb+srv://harinv18:zPWAzE6dMaLy6JpU@product-list.swerw.mongodb.net/";
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB Atlas"))
@@ -49,31 +50,36 @@ app.get("/seed", async (req, res) => {
       name: "Fjallraven Backpack",
       price: 109.95,
       description: 'Fits 15" laptops',
-      image: "https://www.jiomart.com/images/product/original/rvjjz4lfas/astrid-men-s-laptop-bag-office-bag-with-sling-waterproof-laptop-sleeve-cover-brown-7-l-product-images-rvjjz4lfas-0-202312201506.jpg?im=Resize=(600,750)",
+      image:
+        "https://www.jiomart.com/images/product/original/rvjjz4lfas/astrid-men-s-laptop-bag-office-bag-with-sling-waterproof-laptop-sleeve-cover-brown-7-l-product-images-rvjjz4lfas-0-202312201506.jpg?im=Resize=(600,750)",
     },
     {
       name: "Mens Casual Shirt",
       price: 22.3,
       description: "Slim Fit T-Shirts",
-      image: "https://www.jiomart.com/images/product/original/rvzuth3vr6/bruton-trendy-running-shoes-sneaker-shoes-for-men-product-images-rvzuth3vr6-0-202401111155.jpg?im=Resize=(330,410)",
+      image:
+        "https://www.jiomart.com/images/product/original/rvzuth3vr6/bruton-trendy-running-shoes-sneaker-shoes-for-men-product-images-rvzuth3vr6-0-202401111155.jpg?im=Resize=(330,410)",
     },
     {
       name: "Mens Cotton Jacket",
       price: 55.99,
       description: "Stylish cotton jacket",
-      image: "https://www.jiomart.com/images/product/original/rvqfvgju8p/crazy-bee-t-shirt-for-men-men-t-shirt-tshirt-for-men-back-print-tshirt-t-shirts-oversized-tshirt-men-tshirt-tshirt-men-tshirts-for-men-oversized-t-shirt-oversize-t-shirt-oversized-tshirt-men-product-images-rvqfvgju8p-0-202409201137.jpg?im=Resize=(330,410)",
+      image:
+        "https://www.jiomart.com/images/product/original/rvqfvgju8p/crazy-bee-t-shirt-for-men-men-t-shirt-tshirt-for-men-back-print-tshirt-t-shirts-oversized-tshirt-men-tshirt-tshirt-men-tshirts-for-men-oversized-t-shirt-oversize-t-shirt-oversized-tshirt-men-product-images-rvqfvgju8p-0-202409201137.jpg?im=Resize=(330,410)",
     },
     {
       name: "Mens Casual Slim Fit",
       price: 15.99,
       description: "Comfortable slim fit shirt",
-      image: "https://www.jiomart.com/images/product/original/rvxsoqxkml/glito-stylish-graphic-printed-kangaroo-pocket-s-hooded-sweartshirts-men-product-images-rvxsoqxkml-0-202409110209.jpg?im=Resize=(330,410)",
+      image:
+        "https://www.jiomart.com/images/product/original/rvxsoqxkml/glito-stylish-graphic-printed-kangaroo-pocket-s-hooded-sweartshirts-men-product-images-rvxsoqxkml-0-202409110209.jpg?im=Resize=(330,410)",
     },
     {
       name: "Elegant Watch",
       price: 120.49,
       description: "Luxury wristwatch",
-      image: "https://www.jiomart.com/images/product/original/rvwgz5gjfb/acnos-silver-strap-analogue-steel-watch-for-men-pack-of-1-fx437-product-images-rvwgz5gjfb-0-202305050811.jpg?im=Resize=(330,410)",
+      image:
+        "https://www.jiomart.com/images/product/original/rvwgz5gjfb/acnos-silver-strap-analogue-steel-watch-for-men-pack-of-1-fx437-product-images-rvwgz5gjfb-0-202305050811.jpg?im=Resize=(330,410)",
     },
   ];
 
